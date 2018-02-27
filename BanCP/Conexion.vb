5533db72f7bfd3cd644e581270c54f5f2edab12d
Jorge Alberto <jorgeleonfdez@hotmail.com>
2018-02-26 18:57:47 -0700

    se agregó clase conexion

--- BanCP/Conexion.vb
+++ BanCP/Conexion.vb
@@ -1,6 +1,19 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class Conexion
-    Dim con As New SqlConnection(My.Settings.Conexion)
+    Private cn As SqlConnection
+    Private cmd As SqlCommand
+    Private dr As SqlDataReader
 
+    Public Sub New()
+        Try
+            Me.cn = New SqlConnection(My.Settings.Conexion)
+            Me.cn.Open()
+        Catch e As Exception
+            MessageBox.Show("Ha habido un error al intentar conectar a la base de datos" + e.Message, "Error de Conexion", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    'metodos a agregar :)
 End Class
