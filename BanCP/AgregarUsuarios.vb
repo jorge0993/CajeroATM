2b9fc0e8381cfaae55cc25c966c790e1201adea5
Jorge Alberto <jorgeleonfdez@hotmail.com>
2018-02-26 20:10:29 -0700

    Se agregó metodo para mostrar usuarios

--- BanCP/AgregarUsuarios.vb
+++ BanCP/AgregarUsuarios.vb
@@ -1,3 +1,7 @@
 ﻿Public Class AgregarUsuarios
+    Dim con = New Conexion
 
+    Private Sub AgregarUsuarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.mostrarUsuarios(dgUsuarios)
+    End Sub
 End Class
