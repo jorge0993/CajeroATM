1d4a6341896063b0efac3ae3593771c6ca592625
roberto02 <roberto.ues02@gmail.com>
2018-02-26 21:42:56 -0700

    agregando labels

--- BanCP/AgregarUsuarios.vb
+++ BanCP/AgregarUsuarios.vb
@@ -4,4 +4,32 @@
     Private Sub AgregarUsuarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         con.mostrarUsuarios(dgUsuarios)
     End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
