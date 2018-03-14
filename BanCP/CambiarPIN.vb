e477197f3c4c35b086152972bba0686a6974ec92
Hector Rodriguez <hecttor201@gmail.com>
2018-03-14 11:11:38 -0700

    Ya quedo cambiar pin

--- BanCP/CambiarPIN.vb
+++ BanCP/CambiarPIN.vb
@@ -1,6 +1,7 @@
 ﻿Public Class CambiarPIN
     Dim con = New Conexion
     Dim fun = New Funciones
+    Dim validar As Boolean = False
     Private Sub tbPIN_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tbPIN.KeyPress
         fun.validacionKeyNum(e)
     End Sub
@@ -45,5 +46,20 @@
         Dim menu As New Menu
         Me.Hide()
         menu.Show()
+    End Sub
+
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        If (tbNuevo.Text = tbConfirmar.Text) Then
+            If (tbNuevo.Text <> "") Then
+                validar = con.ValidarPIN(Convert.ToInt32(tbPIN.Text))
+                MessageBox.Show(con.CambiarPIN((Convert.ToInt32(tbNuevo.Text)), validar))
+            Else
+                MessageBox.Show("El nuevo PIN no puede estar vacio", "PIN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            End If
+        Else
+            MessageBox.Show("El pin no coincide", "PIN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            tbNuevo.Text = ""
+            tbConfirmar.Text = ""
+        End If
     End Sub
 End Class
